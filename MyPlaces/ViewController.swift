41cf68b4084fabd9434b86ff0bb3d87ddc1de7af
dvoineu <av.dvoinev@gmail.com>
2020-07-11 11:32:51 +0200

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Aliaksandr Dvoineu on 11.07.20.
+//  Copyright © 2020 Aliaksandr Dvoineu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
