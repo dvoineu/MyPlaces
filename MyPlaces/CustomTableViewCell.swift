03161c108d09b5defce5ca2f01ec4a6f7ecff356
dvoineu <av.dvoinev@gmail.com>
2020-07-11 13:51:57 +0200

    Custom View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Aliaksandr Dvoineu on 11.07.20.
+//  Copyright © 2020 Aliaksandr Dvoineu. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    @IBOutlet weak var locationLbl: UILabel!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    
+    
+}
